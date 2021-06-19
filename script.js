7489d0be16da8756d300ef855e240218ded30e7a
Faycel Benyoussa <faycel@ouvrages-web.fr>
2021-06-19 14:37:46 +0200

    Add count down and remove auto join

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-const redirect = (source1, source2) => {
+let TIME_BEFORE_SWITCHING = 10;
+const source1 = 'https://whereby.com/ouvrages.io';
+const source2 = 'https://whereby.com/ouvrages';
+
+const countDown = () => {
+  if (TIME_BEFORE_SWITCHING === 0) redirect();
+
+  let countElement;
+  let bigCountElement;
+  if (TIME_BEFORE_SWITCHING === 10) {
+    countElement = document.createElement('div');
+    bigCountElement = document.createElement('span');
+    countElement.classList = 'section-3_oy count-down';
+    bigCountElement.classList = 'big-count-down';
+
+    const roomStatusBar = document.querySelector(
+      '.jstest-room-statusbar.settingsButton-2fU_'
+    );
+    const app = document.getElementById('app');
+
+    roomStatusBar.appendChild(countElement);
+    app.appendChild(bigCountElement);
+  } else {
+    countElement = document.querySelector('.section-3_oy.count-down');
+    bigCountElement = document.querySelector('.big-count-down');
+  }
+
+  countElement.innerText = `Redirection dans : ${TIME_BEFORE_SWITCHING}s`;
+  bigCountElement.innerText = TIME_BEFORE_SWITCHING;
+  TIME_BEFORE_SWITCHING--;
+};
+
+const redirect = () => {
   if (window.location.href === source1) {
     self.location = source2;
   } else if (window.location.href === source2) {
@@ -6,31 +38,23 @@ const redirect = (source1, source2) => {
   }
 };
 
-const switchRoom = (source1, source2) => {
+const switchRoom = () => {
   const timeLeftMessage = document.querySelector('.trimmable-L37n');
-  const joinButton = document.querySelector(
-    '.jstest-join-room-button.Button-1doF.Button--full-3rLE'
-  );
-
   if (timeLeftMessage) {
     const fullTime = timeLeftMessage.textContent.split(' ').slice(-2);
     const minutesLeft = parseInt(fullTime[0], 10);
     // NaN = minutesLeft < 1min
     if (isNaN(minutesLeft)) {
-      redirect(source1, source2);
+      countDown();
     }
-  } else if (joinButton) {
-    joinButton.click();
   }
 };
 
 const main = () => {
-  const source1 = 'https://whereby.com/ouvrages.io';
-  const source2 = 'https://whereby.com/ouvrages';
   window.addEventListener(
     'load',
     () => {
-      setInterval(() => switchRoom(source1, source2), 1000);
+      setInterval(() => switchRoom(), 1000);
     },
     false
   );
